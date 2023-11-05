12114c233672f2bc3f26f11feb7f4fb16589a67b
SAJJAK ALI <112471219+imSAJJAKALI@users.noreply.github.com>
2023-11-05 22:09:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
